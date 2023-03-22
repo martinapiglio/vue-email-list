ae890de487ee99b830267a7cc10ebaa1dea06aa8
martinapiglio <martina.piglio@gmail.com>
2023-03-22 14:03:38 +0100

    ten random emails list created

--- js/script.js
+++ js/script.js
@@ -3,7 +3,36 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'test'
+      randomEmailsArray: []
     }
+  },
+
+  created() {
+    this.randomArray(10);
+  },
+
+  methods: {
+
+    randomArray(arrayLenght) {
+      
+        for (let i = 0; i < arrayLenght; i++) {
+        
+            let randomMail; 
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+
+                randomMail = res.data.response;
+
+                if (!this.randomEmailsArray.includes(randomMail)) {
+                this.randomEmailsArray.push(randomMail);
+                };
+        
+            });
+
+        };
+
+    }
+
   }
+
 }).mount('#app')
